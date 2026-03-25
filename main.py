416503e396c2ff1fabf3e6ad9cd5c4f8960a6ab4
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-25 18:28:37 -0400

    code cleanup and rename file for clarity

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ from file_manager import FileManager
 # Initialize the file_manager
 file_manager = FileManager()
 
-# Retrieve invite list and set it equal to a variable
+# Retrieve invite list and set it equal to a variable as a list
 invited_names_content = file_manager.read_file(relative_path="Input/Names/invited_names.txt")
 invite_list = invited_names_content.split("\n")
 # print(invite_list)
 
 # Retrieve template letter and set it equal to a variable
-starting_letter_content = file_manager.read_file(relative_path="Input/Letters/starting_letter.txt")
+letter_template_content = file_manager.read_file(relative_path="Input/Letters/letter_template.txt")
 # print(starting_letter_content)
 
 for each_name in invite_list:
     # Replace each "[name]" string with the list of names and write each new_letter to a directory
-    new_letter_content = starting_letter_content.replace("[name]", each_name)
+    new_letter_content = letter_template_content.replace("[name]", each_name)
     # print(new_letter_content)
     new_filename = each_name + "_invite.txt"
     # print(new_letter_filename)
